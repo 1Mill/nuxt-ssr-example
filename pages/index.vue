0c30eb1a7eb4feaabf4689a9dddc000a0f01e619
1Mill <erik.ekberg@outlook.com>
2018-08-22 18:01:36 -0700

    Test that axios API works
    Axios is enabled, however, a base URL needs to bet set in the config as to not have to call 'https://.../posts' every time, instead just use '/posts' within the call.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-		Index
+		<h1>Index Page</h1>
+		{{ posts }}
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+import Post from '~/components/Post'
+
 export default {
+	async asyncData({ req, params }) {
+		return axios.get('https://jsonplaceholder.typicode.com/posts')
+			.then(res => {
+				return { posts: res.data.slice(0, 5) }
+			})
+	},
+	data: function () {
+		return {
+			posts: []
+		}
+	}
 }
 </script>
 
