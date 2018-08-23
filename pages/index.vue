0504c25215d2f647d95222a4c877060c1a4838a2
1Mill <erik.ekberg@outlook.com>
2018-08-23 15:00:06 -0700

    Add link to posts/:id path

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
 		<h1>Index Page</h1>
 		<div v-for='post in posts' :key='post.id'>
+			<router-link :to="'/posts/' + post.id">LINK</router-link>
       {{ post.id }}: {{ post.title }}
     </div>
 	</div>
