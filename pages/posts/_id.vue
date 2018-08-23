1586ce6e266c1347fd4d1ea4992a583d88f4b3b2
1Mill <erik.ekberg@outlook.com>
2018-08-23 15:34:56 -0700

    Get post by id

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,12 +1,21 @@
 <template>
 	<div>
 		<h1>Post page for {{$route.params.id}}</h1>
+		<div>
+			<h2>{{ post.title }}</h2>
+			<p>{{ post.body }}</p>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
-
+	asyncData ({ app, params }) {
+		return app.$axios.$get(`/posts/${params.id}`)
+			.then((res) => {
+				return { post: res }
+			})
+	}
 }
 </script>
 
