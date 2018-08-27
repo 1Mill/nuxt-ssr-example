682e744305159302abb63e3b7f879c3f8e29256a
1Mill <erik.ekberg@outlook.com>
2018-08-27 13:08:08 -0700

    Use vuex to generate post _id template

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,8 +1,9 @@
 <template>
 	<div>
-		<h1>Post page for {{$route.params.id}}</h1>
+		<router-link to='/'>HOME</router-link>
+		<h1>Post page for {{ $route.params.id }}</h1>
 		<div v-if='post'>
-			<h2>{{ post.title }}</h2>
+			<h2>ID: {{ post.id }} -- {{ post.title }}</h2>
 			<p>{{ post.body }}</p>
 		</div>
 		<div v-else>
@@ -12,12 +13,15 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-	asyncData ({ app, params }) {
-		return app.$axios.$get(`/posts/${params.id}`)
-			.then((res) => {
-				return { post: res }
-			})
+	async fetch ({ store, params }) {
+		await store.dispatch('getPostById', params.id)
+	},
+
+	computed: {
+		...mapGetters([ 'post' ])
 	}
 }
 </script>
