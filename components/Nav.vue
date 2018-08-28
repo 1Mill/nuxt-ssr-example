3799ec21c381135861f763d8a1f8779a233cb1aa
1Mill <erik.ekberg@outlook.com>
2018-08-28 16:01:57 -0700

    Add Nav element to top of layout

---
+++ components/Nav.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class='[ w-100 pa3 white bg-dark-gray ]'>
+		NAV
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class='[ ph1 ]'>
-    <nuxt/>
+  <div>
+	<Nav/>
+    <nuxt class='[ ph1 ]'/>
   </div>
 </template>
+
+<script>
+import Nav from '~/components/Nav'
+
+export default {
+	components: {
+		Nav
+	}
+}
+</script>
 
 <style>
 </style>
