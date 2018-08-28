56a55962fb0f399ea857aa1031129e2c97b2882f
1Mill <erik.ekberg@outlook.com>
2018-08-28 16:11:49 -0700

    Toggle active content on click

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,10 +1,26 @@
 <template>
 	<div class='[ w-100 pa3 white bg-dark-gray ]'>
-		NAV
+		<div @click='toggleActive'>NAV</div>
+		<div v-if='active'>
+			CONTENT
+		</div>
 	</div>
 </template>
 
 <script>
+export default {
+	data: function () {
+		return {
+			active: false
+		}
+	},
+
+	methods: {
+		toggleActive: function () {
+			this.active = !this.active
+		}
+	}
+}
 </script>
 
 <style>
